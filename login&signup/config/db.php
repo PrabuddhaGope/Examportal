a4da64c597ed144101180186d1fb5d977202fe8f
Prabuddha <prabuddhag1>
2021-01-02 17:23:18 +0530

    Change user name and password on db.php

--- login&signup/config/db.php
+++ login&signup/config/db.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
 
 
-$db_connect = mysqli_connect('localhost','amit','amit2424','portal_data');
+// $db_connect = mysqli_connect('localhost','amit','amit2424','portal_data');
+$db_connect = mysqli_connect('localhost','ecell_neo','4epl2L5^','portal_data');
 
 $tablename = 'user_login_data';
 		if (!$db_connect) {
 			echo 'Connection error' . mysqli_connect_error();
-			
+
 			echo "Something Went Wrong!";
 		}
 
--- login&signup/db.php
+++ login&signup/db.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
 
 
-	$db_connect = mysqli_connect('localhost','amit','amit2424','portal_data');
+	$db_connect = mysqli_connect('localhost','ecell_neo','4epl2L5^','portal_data');
 
-	
+
+
 
 		if (!$db_connect) {
-			
+
 			echo "Something Went Wrong!";
 		}
 
